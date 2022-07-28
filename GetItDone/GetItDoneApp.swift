039473a2ffc74643aec83ecc9b57a45fef7ef710
jhsee5 <julietgithubsierra5@gmail.com>
2022-07-28 09:10:55 +0300

    Initial Commit

---
+++ GetItDone/GetItDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetItDoneApp.swift
+//  GetItDone
+//
+//  Created by Jian Hui See on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GetItDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
