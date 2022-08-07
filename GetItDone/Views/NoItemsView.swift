b6edcdef7843af13af983892796d2a7c664edf0f
jhsee5 <julietgithubsierra5@gmail.com>
2022-08-07 17:16:24 +0300

    8:40 / 22:31 User Experience and Animations in SwiftUI | Todo List #5

--- GetItDone/Views/NoItemsView.swift
+++ GetItDone/Views/NoItemsView.swift
@@ -14,8 +14,19 @@ struct NoItemsView: View {
                 Text("There are no items!")
                     .font(.title)
                     .fontWeight(.semibold)
-                
+                Text("Are you a productive person? I think you should click on the add button and add a bunch of items to your todo list! 📝")
+                NavigationLink(destination: AddVIew(), label: {
+                    Text("Add something 🥳")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
             }
+            .multilineTextAlignment(.center)
+            .padding(40)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
